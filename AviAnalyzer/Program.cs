230f0c6657a73c3ca0db874aa2f84888ebb5589f
ShTair <git@vsh.jp>
2018-08-09 20:05:46 +0900

    RIFFの解析を実装

--- AviAnalyzer/Program.cs
+++ AviAnalyzer/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AviAnalyzer
 {
     class Program
     {
+        private static byte[] _buffer = new byte[4];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run(args[0]).Wait();
+        }
+
+        private static async Task Run(string target)
+        {
+            using (var stream = File.Open(target, FileMode.Open))
+            {
+                await ReadChunk(stream, 0, "");
+            }
+        }
+
+        private static async Task<int> ReadChunk(Stream stream, int offset, string indent)
+        {
+            stream.Seek(offset, SeekOrigin.Begin);
+            await stream.ReadAsync(_buffer, 0, 4);
+            var id = Encoding.ASCII.GetString(_buffer);
+
+            await stream.ReadAsync(_buffer, 0, 4);
+            var length = BitConverter.ToInt32(_buffer, 0);
+
+            if (id.Equals("RIFF", StringComparison.CurrentCultureIgnoreCase) || id.Equals("LIST", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.Write($"{indent}{id}");
+                return 8 + await ReadCapsule(stream, offset + 8, length, indent);
+            }
+            else
+            {
+                Console.Write($"{indent}{id}");
+                Console.CursorLeft = 16;
+                Console.WriteLine($"({Pad8(offset)} {Pad8(offset + 8)} -> {Pad8(offset + 8 + length)})({Pad8(length)})");
+                return 8 + length;
+            }
+        }
+
+        private static async Task<int> ReadCapsule(Stream stream, int offset, int length, string indent)
+        {
+            stream.Seek(offset, SeekOrigin.Begin);
+            await stream.ReadAsync(_buffer, 0, 4);
+            var id = Encoding.ASCII.GetString(_buffer);
+
+            Console.Write($"({id})");
+            Console.CursorLeft = 16;
+            Console.WriteLine($"({Pad8(offset - 8)} {Pad8(offset + 4)} -> {Pad8(offset + length)})({Pad8(length - 4)})");
+
+            var current = 4;
+            while (current < length)
+            {
+                current += await ReadChunk(stream, offset + current, indent + " ");
+            }
+
+            return length;
+        }
+
+        private static string Pad8(int num)
+        {
+            return num.ToString("X2").PadLeft(8);
         }
     }
 }
