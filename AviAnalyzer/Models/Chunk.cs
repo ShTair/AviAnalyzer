bf8f06e8f653cf475c9ea5f03d7df62a7c113956
ShTair <git@vsh.jp>
2018-08-10 14:35:34 +0900

    モデルでの作成完成

--- AviAnalyzer/Models/Chunk.cs
+++ AviAnalyzer/Models/Chunk.cs
@@ -2,11 +2,27 @@
 
 namespace AviAnalyzer.Models
 {
-    class Chunk : Data
+    class Chunk
     {
-        public Chunk(Stream stream, int offset, int length, string fourCC) : base(stream, offset, length)
+        protected Stream _stream;
+
+        protected int _offset;
+
+        protected int _length;
+
+        public string FourCC { get; }
+
+        public Chunk(Stream stream, int offset, int length, string fourCC)
         {
+            _stream = stream;
+            _offset = offset;
+            _length = length;
             FourCC = fourCC;
+        }
+
+        public override string ToString()
+        {
+            return $"{FourCC}: {_offset:X2} -> {_offset + _length + 8:X2}({_length + 8:X2}): {_offset + 8:X2}({_length:X2})";
         }
     }
 }
--- AviAnalyzer/Models/List.cs
+++ AviAnalyzer/Models/List.cs
@@ -1,51 +1,65 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace AviAnalyzer.Models
 {
-    class List : Data
+    class List : Chunk
     {
         public string ListFourCC { get; }
 
-        public List<Data> Datas { get; }
+        public List<Chunk> Chunks { get; }
 
-        public List(Stream stream, int offset, int length, string listFourCC) : base(stream, offset, length) { }
+        public List(Stream stream, int offset, int length, string fourCC, string listFourCC) : base(stream, offset, length, fourCC)
+        {
+            ListFourCC = listFourCC;
+            Chunks = new List<Chunk>();
+        }
 
         public static async Task<List> Parse(Stream stream)
         {
-            var fourCC = await stream.ReadFourCCAsync(0);
+            var listFourCC = await stream.ReadFourCCAsync(0);
             var length = await stream.ReadInt32Async(4);
+            var fourCC = await stream.ReadFourCCAsync(8);
 
-            var list = new List(stream, 0, length, fourCC);
-            await list.ParseChildren(stream);
+            var list = new List(stream, 0, length, fourCC, listFourCC);
+            Console.WriteLine(list);
+            await list.ParseChildren(stream, " ");
             return list;
         }
 
-        private async Task ParseChildren(Stream stream)
+        private async Task ParseChildren(Stream stream, string indent)
         {
-            FourCC = await stream.ReadFourCCAsync(_offset + 8);
-
             int current = 4;
             while (current < _length)
             {
-                var fourCC = await stream.ReadFourCCAsync(_offset + current);
-                var length = await stream.ReadInt32Async(_offset + current + 4);
+                var fourCC = await stream.ReadFourCCAsync(_offset + 8 + current);
+                var length = await stream.ReadInt32Async(_offset + 8 + current + 4);
 
                 if (fourCC == "RIFF" || fourCC == "LIST")
                 {
-                    var list = new List(stream, _offset + current, length, fourCC);
-                    Datas.Add(list);
-                    await list.ParseChildren(stream);
+                    var realFourCC = await stream.ReadFourCCAsync(_offset + 8 + current + 8);
+                    var list = new List(stream, _offset + 8 + current, length, realFourCC, fourCC);
+                    Console.WriteLine(indent + list);
+
+                    await list.ParseChildren(stream, indent + " ");
+                    Chunks.Add(list);
                 }
                 else
                 {
-                    var chunk = new Chunk(stream, _offset + current, length, fourCC);
-                    Datas.Add(chunk);
+                    var chunk = new Chunk(stream, _offset + 8 + current, length, fourCC);
+                    Console.WriteLine(indent + chunk);
+                    Chunks.Add(chunk);
                 }
 
                 current += 8 + length;
             }
+        }
+
+        public override string ToString()
+        {
+            return $"{FourCC}({ListFourCC}): {_offset:X2} -> {_offset + _length + 8:X2}({_length + 8:X2}): {_offset + 12:X2}({_length - 4:X2})";
         }
     }
 }
